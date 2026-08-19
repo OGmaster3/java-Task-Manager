2c2c13f789bb711c662205423222655639e95da1
Frank Valdez <fvaldez2166@gmail.com>
2026-08-19 13:28:23 -0500

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,33 @@
+// Task.java to makes private list and tasks
+public class Task{
+
+    private String list;
+
+    private int tasks;
+
+ // Default constructor
+public Task(){
+    this.list = "";
+    this.tasks =0;
+}
+
+// Getter for list name
+public String getList{
+    return list;
+}
+// Getter for number of tasks
+public int getTasks{
+    return tasks;
+}
+
+// Serrer for list name
+public void setList(String list){
+    this.list = list;
+}
+
+// Setter for number of tasks
+public void setTasks(int tasks){
+    this.tasks = tasks;
+}
+
+}
